f1859ab63478b9ae16cf2e49d22fc6c9cdad436a
nethanielle <82792369+nethanielle@users.noreply.github.com>
2021-10-01 23:35:41 +0800

    ✅ added hover and click SFX + Coroutine

--- Sin/Assets/TitleScreenAssets/Scripts/TSPlay.cs
+++ Sin/Assets/TitleScreenAssets/Scripts/TSPlay.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TSPlay : MonoBehaviour
 {
+
     public void PlayButton()
     {
+        StartCoroutine(delayTime());
+    }
+
+    private IEnumerator delayTime()
+    {
+        yield return new WaitForSeconds(1.0f);
         SceneManager.LoadScene("RoomOne");
+
     }
 
 }
