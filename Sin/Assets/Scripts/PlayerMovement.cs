777f6d94392dae01339d7652b98c00afba9b2239
elliedotjpg <ellieacharon@gmail.com>
2021-10-02 19:27:42 +0800

    ✅ Fixed/Redid 'PlayerMovement.cs' script

--- Sin/Assets/Scripts/PlayerMovement.cs
+++ Sin/Assets/Scripts/PlayerMovement.cs
@@ -4,83 +4,38 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     private GameManager gameManager = new GameManager();
 
-    public float MovementSpeed = 1;
+    public float MovementSpeed = 1f;
 
-    private Rigidbody2D _rigidbody;
-    private Animator _animator;
-    public int Score;
+    public Rigidbody2D rb;
+    public Animator _animator;
 
-    AudioSource ButtonSFX;
+    Vector2 movement;
 
-    private void Start()
-    {
-        _rigidbody = GetComponent<Rigidbody2D>();
-        _animator = GetComponent<Animator>();
-    }
+    //AudioSource ButtonSFX;
 
-    private void Update()
+    private void StartGame()
     {
         if (GameManager.GameState == GameManager.EnumGameState.Playing)
         {
-            PlayerInput();
-        }
-        else
-        {
-            _rigidbody.velocity = new Vector2(0, 0);
-        }
-        _animator.SetFloat("WalkHorizontal", Mathf.Abs(_rigidbody.velocity.x));
-        _animator.SetFloat("WalkVerticalUp", Mathf.Abs(_rigidbody.velocity.y));
-        //_animator.SetFloat("WalkVerticalDown", Mathf.Abs(_rigidbody.velocity.y));
-        Debug.Log(_rigidbody.velocity);
-    }
-
-    private void PlayerInput()
-    {
-        transform.parent = null;
-
-
-        // Down
-        if (Input.GetKey(KeyCode.S))   
-        {
-            SetMovement(-MovementSpeed, Mathf.Abs(transform.localScale.x));
-        }
-
-        // Up
-        else if (Input.GetKey(KeyCode.W))
-        {
-            SetMovement(MovementSpeed, Mathf.Abs(transform.localScale.x));
-        }
-
-        else
-        {
-            _rigidbody.velocity = new Vector2(0, _rigidbody.velocity.y);
-
-        }
-
-
-        // Right
-        if (Input.GetKey(KeyCode.D))
-        {
-            SetMovement(MovementSpeed, Mathf.Abs(transform.localScale.y));
-        }
-
-        // Left
-        else if (Input.GetKey(KeyCode.A))
-        {
-            SetMovement(-MovementSpeed, -Mathf.Abs(transform.localScale.y));
-        }
-        else
-        {
-            _rigidbody.velocity = new Vector2(0, _rigidbody.velocity.x);
+            FixedUpdate();
         }
     }
 
-    private void SetMovement(float MovementSpeed, float faceDirection)
+    void Update()
     {
-        _rigidbody.velocity = new Vector2(MovementSpeed, _rigidbody.velocity.y);
-        transform.localScale = new Vector3(faceDirection, transform.localScale.y, transform.localScale.z);
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        _animator.SetFloat("WalkHorizontalRight", movement.x);
+        _animator.SetFloat("WalkHorizontalLeft", -movement.x);
+        _animator.SetFloat("WalkVerticalDown", -movement.y);
+        _animator.SetFloat("WalkVerticalUp", movement.y);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * MovementSpeed * Time.fixedDeltaTime);
     }
 }
