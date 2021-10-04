4b5159b70f5665ff695b6dce65dd53bb45d031a0
elliedotjpg <ellieacharon@gmail.com>
2021-10-04 21:03:21 +0800

    🔄️ 'MotherScript.cs' script for Mother (NPC) in the works!

--- Sin/Assets/Scripts/MotherScript.cs
+++ Sin/Assets/Scripts/MotherScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MotherScript : MonoBehaviour
 {
+    [SerializeField] private PlayerInventory playerInventory;
+    [SerializeField] private InventoryItem thisItem;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,14 @@ public class MotherScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void TriggerMother()
+    {
+        if (playerInventory.myInventory.Contains(thisItem))
+        {
+
+        }
     }
 }
