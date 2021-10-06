4516c1ab1440cea47705e813c48816d29191b6ba
elliedotjpg <ellieacharon@gmail.com>
2021-10-06 16:38:05 +0800

    ⚠️ Issue with loading next scene through opened door

--- Sin/Assets/Scripts/Door/DoorController1.cs
+++ Sin/Assets/Scripts/Door/DoorController1.cs
@@ -1,22 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DoorController1 : MonoBehaviour
 {
-    public Collider2D collider;
+    public GameObject EnterDoor;
+
+    //public new Collider2D collider;
     public bool isOpen;
     public Animator animator;
 
+    private void Start()
+    {    
+        //collider = GetComponent<Collider2D>();
+        EnterDoor.SetActive(false);;
+    }
     public void OpenDoor()
     {
         if (!isOpen)
         {
             isOpen = true;
+            EnterDoor.SetActive(true);
+
             UnityEngine.Debug.Log("Door unlocked!");
             animator.SetBool("isOpen", isOpen);
+        }
+    }
 
-            collider.enabled = !collider.enabled;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log("Collider Enabled = " + GetComponent<Collider>().enabled);
+        if (isOpen == true)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                SceneManager.LoadScene(2);
+            }
         }
     }
 }
